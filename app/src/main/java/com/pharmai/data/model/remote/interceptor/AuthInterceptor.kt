33fdb02742a0cbbc39d6d9fc3befeedf86b10646
Mathew <matthewcharles.tech@gmail.com>
2026-04-09 09:59:01 +0530

    Auth Interceptor

---
+++ app/src/main/java/com/pharmai/data/model/remote/interceptor/AuthInterceptor.kt
@@ -0,0 +1,24 @@
+package com.pharmai.data.model.remote.interceptor
+
+import com.pharmai.core.common.Constants
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class AuthInterceptor : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val original = chain.request()
+        val originalUrl = original.url
+
+        val url = originalUrl.newBuilder()
+            .addQueryParameter("api_key", Constants.ApiKeys.OPENFDA_API_KEY)
+            .build()
+
+        val request = original.newBuilder()
+            .url(url)
+            .header("Accept", "application/json")
+            .header("Content-Type", "application/json")
+            .build()
+
+        return chain.proceed(request)
+    }
+}
