f88772a8b494c063dabcff131fcbb13fab8e1c1d
Mathew <matthewcharles.tech@gmail.com>
2026-04-14 20:28:42 +0530

    Application

--- app/src/main/java/com/pharmai/PharmAiApplication.kt
+++ app/src/main/java/com/pharmai/PharmAiApplication.kt
@@ -1,12 +1,33 @@
 package com.pharmai
 
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.compose.rememberNavController
+//import com.pharmai.core.Navigation
+import com.pharmai.core.theme.PharmAiTheme
+//import com.pharmai.presentation.theme.PharmAiTheme
+import dagger.hilt.android.AndroidEntryPoint
 
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class PharmAiApplication : Application() {
-    override fun onCreate() {
-        super.onCreate()
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        installSplashScreen()
+        setContent {
+            PharmAiTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                 //   Navigation.NavGraph(navController = rememberNavController())
+                }
+            }
+        }
     }
 }
